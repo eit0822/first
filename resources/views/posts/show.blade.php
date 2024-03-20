cfd261de929db0de13e47e34d4b301bae5462911
eit0822 <ossa9808@gmail.com>
2024-03-20 22:06:43 +0900

    Add edit

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -15,6 +15,9 @@
                 <h2 class='title'>{{ $post->title }}</h2>
                 <p class='body'>{{ $post->body }}</p>
             </div>
+            <div class='edit'>
+                <a href="/posts/{{ $post->id }}/edit">edit</a>
+            </div>
             <div class='footer'>
                 <a href="/">戻る</a>
             </div>
